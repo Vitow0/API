6df1c4dc8c11fa63f41560a5f8990f30b52ebebc
xXAgentMUXx <mathys.urban@ynov.com>
2025-01-06 12:29:00 +0100

    GroupieTracker Mathys

--- web/js/go_up.js
+++ web/js/go_up.js
@@ -1,9 +1,10 @@
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// when the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+// funcion to display the button
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
